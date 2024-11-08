ba4c15e7d426626379ce0600f78d2b7ab1bc11a8
Victor-Hugo1 <victor.hsouza@sptech.school>
2024-11-07 21:59:25 -0300

    update sobre

--- public/script.js
+++ public/script.js
@@ -11,16 +11,16 @@ window.addEventListener("scroll", function() {
 
 
     document.addEventListener("DOMContentLoaded", function () {
-        // Aplica o efeito de fade-in e blur ao carregar a página
-        document.body.classList.add("page-transition-in");
+        
+        document.body.classList.add("transicao-blur-entrando");
     });
     
     function navigateTo(url) {
-        // Aplica o efeito de fade-out com blur antes de sair da página
-        document.body.classList.add("page-transition-out");
+       
+        document.body.classList.add("transicao-blur-saindo");
     
         setTimeout(function () {
             window.location.href = url;
-        }, 800); // Sincroniza com o tempo da animação fadeOutBlur (0.8s)
+        }, 400); 
     }
     
