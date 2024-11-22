bea93e838448461b1ec9d23f791cc3b8907c6044
Victor-Hugo1 <victor.hsouza@sptech.school>
2024-11-21 22:57:35 -0300

    update game

--- public/dashboard/dash.js
+++ public/dashboard/dash.js
@@ -1,33 +1,51 @@
 const ctx = document.getElementById('meuGrafico').getContext('2d');
 
-// Cria o gráfico
+
+
 const meuGrafico = new Chart(ctx, {
-    type: 'bar', // Tipo de gráfico (pode ser 'line', 'pie', 'doughnut', etc.)
+  
+    type: 'bar',
     data: {
-        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio'], // Rótulos do eixo X
+        labels: ['Gigi', 'Outro', 'Mais Um'],
         datasets: [{
-            label: 'Vendas em R$', // Legenda do gráfico
-            data: [12, 19, 3, 5, 2], // Dados do gráfico
+            label: 'Ranking',
+            data: [12, 19, 3, 5, 2],
+            
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)'
+                'rgba(233,111,5, 0.5)',
+                'rgba(233,111,5, 0.5)',
+                'rgba(233,111,5, 0.5)',
+                'rgba(233,111,5, 0.5)',
+                'rgba(233,111,5, 0.5)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)'
+                'rgba(233,111,5, 1)',
+                'rgba(233,111,5, 1)',
+                'rgba(233,111,5, 1)',
+                'rgba(233,111,5, 1)',
+                'rgba(233,111,5, 1)'
             ],
-            borderWidth: 1 // Espessura da borda
+            borderWidth: 2,
+            borderRadius:10
         }]
     },
     options: {
         scales: {
+            x: {
+                ticks: {
+                    display: false
+                },
+                grid: {
+                    display: false // Remove as linhas de grade do eixo X
+                }
+            },
             y: {
+                ticks:{
+                    display:false
+                },
+                grid: {
+                    display: false // Remove as linhas de grade do eixo Y
+                },
                 beginAtZero: true // O eixo Y começa no zero
             }
         }
--- public/pac-dog/dog-script.js
+++ public/pac-dog/dog-script.js
@@ -1,42 +1,66 @@
-
-const cachorro = document.querySelector('.cachorro')
-const hidrante = document.querySelector('.hidrante')
-const score = document.querySelector('.score')
-var jaPulou = false
 var contar = 0;
+var intervaloPontuacao;
+const gameOver = document.getElementById("card-gameOver");
+gameOver.style.backgroundColor = "transparent";
 
-document.addEventListener('keydown', (e) => {
-    if ((e.code === 'ArrowUp') | (e.code === 'Space')) {
-        pular();
+function ComecarJogo() {
+    const botao = document.getElementById("botao");
+    const game = document.getElementById("game");
+    const gameOver = document.getElementById("card-gameOver");
+    game.style.display = "block";
+    botao.style.display = "none";
+    gameOver.style.backgroundColor = "rgba(0, 0, 0, 0.753)";
+
+    const cachorro = document.querySelector(".cachorro");
+    const hidrante = document.querySelector(".hidrante");
+    const score = document.querySelector(".score");
+    var jaPulou = false;
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === "ArrowUp" || e.code === "Space") {
+            pular();
+        }
+    });
+
+    function pular() {
+        if (!cachorro.classList.contains("pular")) {
+            cachorro.classList.add("pular");
+            jaPulou = true;
+
+            setTimeout(() => {
+                cachorro.classList.remove("pular");
+                jaPulou = false;
+            }, 1100);
+        }
     }
-});
 
-function pular() {
-    if (!cachorro.classList.contains('pular')); {
-        cachorro.classList.add('pular');
-        jaPulou = true;
-
-        setTimeout(() => {
-            cachorro.classList.remove('pular');
-            jaPulou = false;
-        }, 1100);
-    }
+    intervaloPontuacao = setInterval(() => {
+        var cachorroBottom = parseInt(
+            window.getComputedStyle(cachorro).getPropertyValue("bottom")
+        );
+        var hidranteLeft = parseInt(
+            window.getComputedStyle(hidrante).getPropertyValue("left")
+        );
+        if (hidranteLeft > 1 && hidranteLeft < 200 && cachorroBottom <= 2 && !jaPulou) {
+            mostrarResultados();
+            return;
+        }
+        contar++;
+        score.innerHTML = `Pontuação: ${contar}`;
+    }, 5);
 }
 
-setInterval(() => {
-    var cachorroBottom = parseInt(
-        window.getComputedStyle(cachorro).getPropertyValue('bottom')
-    );
-    var hidranteLeft = parseInt(
-        window.getComputedStyle(hidrante).getPropertyValue('left')
-    );
-    if (hidranteLeft > 1 && hidranteLeft < 200 && cachorroBottom <= 2 && !jaPulou) {
-         alert (`Game Over! Sua pontuação foi: ${contar}`);
-        contar = 0;
-    }
-    contar++;
-    score.innerHTML = `Pontuação: ${contar}`;
-}, 5);
+function mostrarResultados() {
+    const hidrante = document.getElementById("hidrante");
+    const gameOver = document.getElementById("card-gameOver");
+    const ptFinal = document.getElementById("pontuacaoFinal");
+
+    clearInterval(intervaloPontuacao);
+    hidrante.style.animation = "none";
+    gameOver.style.zIndex = 1;
+    ptFinal.innerHTML = `Pontuação Final: ${contar}`;
+}
+
 
 
 
